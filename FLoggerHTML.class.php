78d83218e8e3fe805ae036455ff564e4d2947967
Dias Culenga Daniel <diasculengadaniel908@gmail.com>
2026-01-13 12:19:23 +0100

    feat: Implemt class to handle LOG in HTML

---
+++ FLoggerHTML.class.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Implement LOG In HTML
+* */
+class FLoggerHTML extends FLogger{
+ public function write($message){
+  $time = date("Y-m-d H:i:s");
+  $text = "<p>\n";
+  $text. = " <b>$time</b>: \n";
+  $text. = " <i>$message</i> <br>\n";
+  $text. = "</p>\n";
+
+  $handler = fopen($this->filename, 'a');
+  fwrite($handler, $text);
+  fclose($handler);
+ }
+}
+?>
