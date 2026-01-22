a18b0a0550063e488c9287b76d478ebd3ce09ae3
Dias Culenga Daniel <diasculengadaniel908@gmail.com>
2026-01-22 14:50:51 +0100

    feat:Add FRepository class and implement their load method.

---
+++ FRepository.class.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Provem methods to manage collections of objects
+* */
+final class FRepository{
+ private $class;
+
+ function __construct($class){
+  $this->class = $class;
+ }
+
+ function load(FCriteria $criteria){
+  $sql = new FSqlSelect;
+  $sql->addColumn('*');
+  $sql->setEntity($this->class);
+  $sql->setCriteria($criteria);
+
+  if($conn=FTransaction::get()){
+   FTransaction::log($sql->getInstruction());
+
+   if($result){
+    while($row = $result->fetchObject($this->class.'Record')){
+     $results[]=$row;
+    }
+   }
+   return $results;
+  }else{
+   throw new Exception('Do not have active transaction');
+  }
+ }
+}
+?>
